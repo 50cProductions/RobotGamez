a4fea588676384dbd8ad4eb13db7f95d8463c8f2
aytmogani22rac-pixel <aytmogani22rac@gmail.com>
2026-04-22 00:32:53 +0200

    The winning system and transition screen for the next stage have been added

---
+++ robot-game/UI/WinScreen.gd
@@ -0,0 +1,7 @@
+extends  Control
+
+func _on_next_level_button_pressed():
+	get_tree().change_scene_to_file("res://Levels/Test.tscn")
+
+func on_next_level_button_pressed() -> void:
+	pass # Replace with function body.
