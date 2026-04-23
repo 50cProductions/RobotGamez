4592456f9788c83782d97ce69a6a81a983b071fe
Mohit Mandalia <mohitmandalia2008@gmail.com>
2026-04-23 22:19:24 +0530

    fix laser

--- robot-game/Enemy/Abilities/Laser/laser.gd
+++ robot-game/Enemy/Abilities/Laser/laser.gd
@@ -1,26 +1,31 @@
-extends RayCast2D
+extends Node2D
 
 var direction: Vector2
-var max_length: float = 2000.0
+var damage_amount = 10
+var max_length: float = 200
 
 @onready var line_2d = $Line2D
 
 func _ready() -> void:
-	target_position = Vector2(max_length, 0)
-	force_raycast_update()
-	
-	var cast_point = target_position
-	
-	if is_colliding():
-		cast_point = to_local(get_collision_point())
-		var collider = get_collider()
-		if collider and collider.is_in_group("player"):
-			if collider.has_method("take_damage"):
-				collider.take_damage(10, global_position)
+	var end_point = Vector2(max_length, 0)
 	
 	line_2d.add_point(Vector2.ZERO)
-	line_2d.add_point(cast_point)
-
+	line_2d.add_point(end_point)
+	
+	if has_node("HitBox/CollisionShape2D"):
+		var shape = SegmentShape2D.new()
+		shape.a = Vector2.ZERO
+		shape.b = end_point
+		$HitBox/CollisionShape2D.shape = shape
 
 func _on_timer_timeout() -> void:
 	queue_free()
+
+func get_damage_amount():
+	return damage_amount
+
+func _on_hit_box_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
+
+func _on_hit_box_body_entered(body: Node2D) -> void:
+	pass
