6f1aa89d464f93ab70959d1a425dbc901bf7a921
50c Productions <50cproductionsakathebest@gmail.com>
2026-04-25 23:51:59 +0700

    Enemies & BG music

--- robot-game/Enemy/Robo/robo.gd
+++ robot-game/Enemy/Robo/robo.gd
@@ -1,3 +1,14 @@
 extends CharacterBody2D
 
 @export var damage_amount = 40
+
+var original_pos: Vector2
+func _ready() -> void:
+	Taskmanager.computer_hacked.connect(hacked)
+	if !Taskmanager.hacked:
+		original_pos = global_position
+		position = Vector2(100000, 1000000)
+	
+
+func hacked():
+	position = original_pos
